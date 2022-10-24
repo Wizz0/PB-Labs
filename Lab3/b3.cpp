88b130e118728feeebeb7237a95d6556d6970a93
Wizz0 <samgorokhov01@gmail.com>
2022-10-24 18:29:47 +0900

    add b-files

---
+++ Lab3/b3.cpp
@@ -0,0 +1,57 @@
+//b3
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	long long n;
+	int a,b,x,y;
+	cin >> n;
+	for (int i=0; i < 8; i++){
+		if (i==0){
+			a = n % 10;
+			x = a;
+			x+=x;
+		}
+		else if (i > 0){
+			a = n / 10 % 10;
+			x = a;
+			x+=x;
+		}
+	}
+	for (int i=0; i < 8; i++){
+		if (i==0){
+			b = n / 10 % 10;
+			if (b >= 10){
+				b = (b /10) + (b % 10);
+			}
+			y = b;
+			y+=y;
+		}
+		else if (i > 0){
+			b = n / 100 % 10;
+			if (b >= 10){
+				b = (b/10) + (b%10);
+			}
+			y = b;
+			y+=y;	
+		}
+	}
+	
+	if ((x+y) % 10 == 0) {
+		cout << "VALID";
+	}                   
+	else if ((x+y) % 10 != 0){
+		cout << "INVALID";
+	}                                                                                                          
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
