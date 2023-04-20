095319dc0c46eba6a0111cf805f5ba9a15942686
Wizz0 <samgorokhov01@gmail.com>
2023-04-20 19:57:29 +0900

    Update B3.cpp

--- semester1/Lab1/B3.cpp
+++ semester1/Lab1/B3.cpp
@@ -1,22 +1,26 @@
 //B3
+//Составить программу определения номера подъезда и этажа квартиры по заданным
+//номеру квартиры n, количеству этажей в доме m и количеству квартир на лестничной
+//площадке k.
 
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-int
-main ()
+int main ()
 {
   double n, m, k, P, p, e;
   cout << "Number of apartments" << endl;
-  cin >> n;
+  cin >> n; //Номер квартиры
   cout << "How many floors in the house?" << endl;
-  cin >> m;
+  cin >> m; //количество этажей
   cout << "How many apartments in a floor?" << endl;
-  cin >> k;
+  cin >> k; //количество квартир на этаже
   P = m*k;
   p = ceil(n/P);
-  cout << "Entrace = " << p << endl;
+  cout << "Entrace = " << p << endl; //указать подъезд
   e = (n-(P*(p-1)))/k;
   cout << "Floor = " << ceil(e);
+  
+  return 0;
 }
