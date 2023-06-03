9b9c65199d38fdb1a048685a52d410f26ed77850
Semyon Gorokhov <samgorokhov01@gmail.com>
2023-06-03 18:15:04 +0900

    Create a3.cpp

---
+++ semester2/Lab4/a3.cpp
@@ -0,0 +1,54 @@
+//A3
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+class Nonsense_Gen {
+private:
+    int s;
+    vector<string> dictionary;
+    mt19937 gen;
+
+public:
+    Nonsense_Gen() {
+        s = 1;
+        ifstream fin("Cool_dictionary.txt");
+        if (fin.is_open()) {
+            string word;
+            while (fin >> word) {
+                dictionary.push_back(word);
+            }
+            fin.close();
+        }
+        gen.seed(time(nullptr));
+    }
+
+    void generate() {
+        string filename = "nonsense_" + to_string(s) + ".txt";
+        ofstream fout(filename);
+        if (fout.is_open()) {
+            for (int i = 0; i < 10; i++) {
+                int index = gen() % dictionary.size();
+                fout << dictionary[index] << " ";
+            }
+            fout.close();
+            s++;
+        }
+    }
+};
+
+int main() {
+    Nonsense_Gen generator;
+    int n;
+    cout << "How many random things do you need?" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        generator.generate();
+    }
+    return 0;
+}
