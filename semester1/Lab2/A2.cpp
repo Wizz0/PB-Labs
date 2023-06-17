d51b58ab311c22cec32a38460a1fbe14338edd5d
Semyon Gorokhov <samgorokhov01@gmail.com>
2023-06-17 20:06:43 +0900

    Update and rename a2.cpp to A2.cpp

---
+++ semester1/Lab2/A2.cpp
@@ -0,0 +1,24 @@
+//A2
+//Напишите программу, которая вводит с клавиатуры один символ, и выводит DIGIT, этот
+//символ является цифрой, CAPITAL — если за- главной латинской буквой, LOWERCASE — если
+//строчной, и выводит NON-ALPHANUMERIC в противном случае.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char x;
+    cin >> x;
+    if (x>='0' and x<='9') {
+        cout << "DIGIT";
+    }
+    else if (x>='A' and x<='Z'){
+        cout << "CAPITAL";
+    }
+    else if (x>='a' and x<='z'){
+        cout << "LOWERCASE";
+    }
+    else {
+        cout << "NON-ALPHANUMERIC";
+    }
+}
